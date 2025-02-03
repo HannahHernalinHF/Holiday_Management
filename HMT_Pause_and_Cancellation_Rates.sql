209d81748a28bbf693ba4c118cfdf8ca4f2a67dd
HannahHernalinHF <104628783+HannahHernalinHF@users.noreply.github.com>
2025-02-03 12:23:29 +0100

    Update HMT_Pause_and_Cancellation_Rates.sql

--- HMT_Pause_and_Cancellation_Rates.sql
+++ HMT_Pause_and_Cancellation_Rates.sql
@@ -1,5 +1,7 @@
 --- HMT Analysis: Delivery Changes Due to Public Holidays and its Impact on Customer Behavior (with 1-Off changes incorporate) ---
 
+--SELECT DISTINCT country_group, country_code, country_name, entity_name, brand_name FROM global_bi_business.entity_dimension ORDER BY 1,2,3
+
 -- Get the postal codes for a specific public holiday
 WITH VIEW_1 AS (
     SELECT DISTINCT business_unit,
@@ -10,8 +12,8 @@ WITH VIEW_1 AS (
         postal_code,
         REPLACE(SUBSTR(published_time, 1, 10), '-', '') AS published_date
     FROM public_holiday_shift_live.holiday_shift_latest
-    WHERE business_unit IN ('GB')
-    AND origin_date='2024-08-26' --- Public Holiday
+    WHERE business_unit IN ('FR') ---< Update the country
+    AND origin_date='2024-04-01' ---< Update the Public Holiday
 )
 
 
@@ -25,28 +27,33 @@ FROM VIEW_1
 , VIEW_2A AS (
 SELECT *
 FROM (
-    SELECT fk_subscription
+    SELECT country
+        , fk_subscription
         , delivery_wk_4
         , fk_imported_at_date
         , delivery_time
-        , SUBSTR(zip,1,LEN(zip)-3) AS zip_clean
+        , CASE WHEN country IN ('GB') THEN SUBSTR(zip,1,LEN(zip)-3)
+             WHEN country IN ('IE') THEN SUBSTR(zip,1,LEN(zip)-4)
+             WHEN country IN ('NL') AND REPLACE(UPPER(zip), ' ','')=6
+                 THEN SUBSTR(zip,1,LEN(zip)-2)
+             WHEN country IN ('NL') AND zip='NL5042ZK'
+                 THEN LEFT('5042ZK',4)
+            ELSE zip END AS zip
         , ROW_NUMBER() OVER(PARTITION BY fk_subscription ORDER BY fk_imported_at_date) AS rank
     FROM scm_forecasting_model.subscription_forecast_snapshots
-    WHERE fk_imported_at_date BETWEEN 20240726 AND 20240822
-    AND country='GB'
-    --AND delivery_wk_4 BETWEEN '2024-04-26' AND '2024-05-27'-- IS NOT NULL // Removed as it cuts most of the delivery week
-    AND delivery_wk_4 BETWEEN '2024-08-25' AND '2024-08-31'
-    GROUP BY 1,2,3,4,5)
+    WHERE fk_imported_at_date BETWEEN 20240301 AND 20240307 ---< Update the relevant dates
+      AND country IN ('FR') ---< Update the country
+      AND delivery_wk_4 BETWEEN '2024-03-30' AND '2024-04-05' ---< Update the relevant delivery weeks
+    GROUP BY 1,2,3,4,5,6)
 WHERE rank=1
 )
 
-
-/*
+/* This serves as a checker only to identify which relevant delivery weeks and dates to include in VIEW_2A for faster run time^
 SELECT DISTINCT delivery_wk_4
 FROM scm_forecasting_model.subscription_forecast_snapshots
-WHERE country='GB'
-  --AND delivery_wk_4 = '2024-12-26'
-  AND fk_imported_at_date BETWEEN 20241122 AND 20241128
+WHERE country='FR'
+  --AND delivery_wk_4= '2024-04-01'
+  AND fk_imported_at_date BETWEEN 20240301 AND 20240307
 ORDER BY 1
 */
 
@@ -60,13 +67,13 @@ ORDER BY 1
         , c.hellofresh_week
     FROM VIEW_2A AS a
     LEFT JOIN VIEW_1 AS b
-        ON a.zip_clean=b.postal_code
+        ON a.country = b.business_unit
+        AND a.zip = b.postal_code
         AND a.delivery_time = b.origin_option_handle
     LEFT JOIN dimensions.date_dimension AS c
         ON COALESCE(B.target_date,delivery_wk_4) = c.date_string_backwards
 )
 
---SELECT delivery_wk_4, target_date, cOUNT(DISTINCT fk_subscription) FROM VIEW_2B GROUP BY 1,2 ORDER BY 1,2  /*
 
 -- To incorporate 1 off changes
 , VIEW_6 AS (
@@ -79,20 +86,22 @@ SELECT id_subscription_change_schedule AS change_sub_id
      , status
      , REPLACE(SUBSTR(created_at, 1, 10), '-', '') AS created_at --created_at
 FROM dl_bob_live_non_pii.subscription_change_schedule
-WHERE business_unit = 'GB'
+WHERE business_unit IN ('FR') ---< Update the country
   AND week_id >= '2024-W01'
   AND delivery_time IS NOT NULL
 )
 
+
+
 , VIEW_7 AS (
-SELECT DISTINCT a.fk_subscription,
+SELECT DISTINCT a.country,
+                a.fk_subscription,
                 a.delivery_wk_4, --COALESCE(DATE_ADD(CAST(delivery_wk_4 AS DATE), CAST(SUBSTRING(b.changed_delivery_time,4,1) - SUBSTRING(a.delivery_time,4,1) AS INT)),a.delivery_wk_4) AS delivery_wk_4,
                 a.fk_imported_at_date,
-                --a.created_at,
-                --b.changed_delivery_time,
-                --a.delivery_time,
+                b.changed_delivery_time,
+                a.delivery_time,
                 --COALESCE(b.changed_delivery_time,a.delivery_time) AS delivery_time,
-                a.zip_clean,
+                a.zip,
                 a.origin_option_handle,
                 a.target_option_handle,
                 COALESCE(a.target_date,COALESCE(DATE_ADD(CAST(delivery_wk_4 AS DATE), CAST(SUBSTRING(b.changed_delivery_time,4,1) - SUBSTRING(a.delivery_time,4,1) AS INT)),a.delivery_wk_4)) AS target_date,
@@ -102,17 +111,19 @@ SELECT DISTINCT a.fk_subscription,
                 COALESCE(DATE_ADD(CAST(delivery_wk_4 AS DATE), CAST(SUBSTRING(b.changed_delivery_time,4,1) - SUBSTRING(a.delivery_time,4,1) AS INT)),a.delivery_wk_4) AS updated_delivery_date
 FROM VIEW_2B AS a
 LEFT JOIN VIEW_6 AS b
-    ON a.fk_subscription = b.subscription_id
+    ON a.country = b.business_unit
+    AND a.fk_subscription = b.subscription_id
     AND a.hellofresh_week = b.hellofresh_week
---WHERE changed_delivery_time IS NOT NULL AND delivery_time!=changed_delivery_time
-ORDER BY 1
+ORDER BY 2
 )
 
+--SELECT COUNT(fk_subscription) FROM VIEW_7 WHERE updated_delivery_date='2024-10-03' /*AND changed_delivery_time IS NOT NULL/*
 
 -- Categorise the customers
 , VIEW_2C AS (
-    SELECT fk_subscription
-        , zip_clean
+    SELECT country
+        , fk_subscription
+        , zip
         , updated_delivery_date AS original_delivery_date
         , target_date AS new_delivery_date
         , (CASE WHEN updated_delivery_date=target_date THEN 'Cohort A (0)' --- non-shifted: delivery date is on the public holiday
@@ -120,16 +131,14 @@ ORDER BY 1
                 WHEN DATEDIFF(target_date, updated_delivery_date)=1 THEN 'Cohort C (+1)' --- shifted: delivery date is shifted to 1 day later than the public holiday
                 WHEN DATEDIFF(target_date, updated_delivery_date)=-2 THEN 'Cohort D (-2)' --- shifted: delivery date is shifted to 2 days earlier than the public holiday
                 WHEN DATEDIFF(target_date, updated_delivery_date)=2 THEN 'Cohort E (+2)' --- shifted: delivery date is shifted to 2 days later than the public holiday
-                WHEN DATEDIFF(target_date, updated_delivery_date)=-3 THEN 'Cohort F (-3)' --- shifted: delivery date is shifted to 3 days earlier than the public holiday)
-                WHEN DATEDIFF(target_date, updated_delivery_date)=3 THEN 'Cohort G (+3)' --- shifted: delivery date is shifted to 3 days later than the public holiday)
-                --WHEN DATEDIFF(target_date, updated_delivery_date)=-4 THEN 'Cohort H (-4)' --- shifted: delivery date is shifted to 3 days later than the public holiday)
-                --WHEN DATEDIFF(target_date, updated_delivery_date)=4 THEN 'Cohort I (+4)' --- shifted: delivery date is shifted to 3 days later than the public holiday)
+                WHEN DATEDIFF(target_date, updated_delivery_date)=-3 THEN 'Cohort F (-3)' --- shifted: delivery date is shifted to 3 days earlier than the public holiday
+                WHEN DATEDIFF(target_date, updated_delivery_date)=3 THEN 'Cohort G (+3)' --- shifted: delivery date is shifted to 3 days later than the public holiday
             END) AS cohort
-    FROM VIEW_7--VIEW_2B --
-    WHERE updated_delivery_date='2024-08-26' -- only focus on PH day for now
+    FROM VIEW_7--VIEW_2B
+    WHERE updated_delivery_date='2024-04-01' ---< Update the PH
 )
 
-
+--SELECT cohort, COUNT(DISTINCT fk_subscription) FROM VIEW_2C GROUP BY 1 ORDER BY 1 /*
 
 -- Grab the status for these customers from the first date we used (in VIEW_2A CTE)
 -- First set up the unique list of customers
@@ -141,25 +150,29 @@ ORDER BY 1
     CROSS JOIN (
         SELECT fk_imported_at_date
         FROM scm_forecasting_model.subscription_forecast_snapshots
-        WHERE fk_imported_at_date BETWEEN 20240726 AND 20240831
+        WHERE fk_imported_at_date BETWEEN 20240301 AND 20240410  ---< Update the relevant dates
+         AND country IN ('FR')  ---< Update the country
         GROUP BY 1) AS B
     ORDER BY 1,2
 )
 
+
 -- Third, pull info for the week the PH falls on, moving WOW
 , VIEW_3B AS (
-    SELECT A.*
-        , (CASE WHEN DATEDIFF(TO_DATE(CAST(A.fk_imported_at_date AS STRING), 'yyyyMMdd'), '2024-07-26') BETWEEN 0 AND 6 THEN status_wk_4
-                WHEN DATEDIFF(TO_DATE(CAST(B.fk_imported_at_date AS STRING), 'yyyyMMdd'), '2024-07-26') BETWEEN 7 AND 13 THEN status_wk_3
-                WHEN DATEDIFF(TO_DATE(CAST(B.fk_imported_at_date AS STRING), 'yyyyMMdd'), '2024-07-26') BETWEEN 14 AND 20 THEN status_wk_2
-                WHEN DATEDIFF(TO_DATE(CAST(B.fk_imported_at_date AS STRING), 'yyyyMMdd'), '2024-07-26') BETWEEN 21 AND 27 THEN status_wk_1
-                WHEN DATEDIFF(TO_DATE(CAST(B.fk_imported_at_date AS STRING), 'yyyyMMdd'), '2024-07-26') BETWEEN 28 AND 34 THEN status_wk_0
+    SELECT B.country
+        , A.*
+          ---v Update the start relevant start date to include in the analysis:
+        , (CASE WHEN DATEDIFF(TO_DATE(CAST(A.fk_imported_at_date AS STRING), 'yyyyMMdd'), '2024-03-01') BETWEEN 0 AND 6 THEN status_wk_4
+                WHEN DATEDIFF(TO_DATE(CAST(B.fk_imported_at_date AS STRING), 'yyyyMMdd'), '2024-03-01') BETWEEN 7 AND 13 THEN status_wk_3
+                WHEN DATEDIFF(TO_DATE(CAST(B.fk_imported_at_date AS STRING), 'yyyyMMdd'), '2024-03-01') BETWEEN 14 AND 20 THEN status_wk_2
+                WHEN DATEDIFF(TO_DATE(CAST(B.fk_imported_at_date AS STRING), 'yyyyMMdd'), '2024-03-01') BETWEEN 21 AND 27 THEN status_wk_1
+                WHEN DATEDIFF(TO_DATE(CAST(B.fk_imported_at_date AS STRING), 'yyyyMMdd'), '2024-03-01') BETWEEN 28 AND 34 THEN status_wk_0
             ELSE 'Other' END) AS status
     FROM VIEW_3A AS A
     LEFT JOIN scm_forecasting_model.subscription_forecast_snapshots AS B
-    ON A.fk_subscription=B.fk_subscription
-    AND A.fk_imported_at_date=B.fk_imported_at_date
-    WHERE B.country='GB'
+        ON A.fk_subscription=B.fk_subscription
+        AND A.fk_imported_at_date=B.fk_imported_at_date
+    WHERE B.country IN ('FR')  ---< Update the country
 )
 
 
@@ -168,7 +181,8 @@ SELECT A.*
      , B.cohort
 FROM VIEW_3B AS A
 LEFT JOIN VIEW_2C AS B
-    ON A.fk_subscription=B.fk_subscription
+    ON A.country = B.country
+    AND A.fk_subscription=B.fk_subscription
 )
 
 
@@ -194,7 +208,6 @@ SELECT a.fk_imported_at_date AS Date
     , COUNT(DISTINCT CASE WHEN a.cohort = 'Cohort E (+2)' THEN a.fk_subscription END) / b.Total_Subs AS Cohort_E
     , COUNT(DISTINCT CASE WHEN a.cohort = 'Cohort F (-3)' THEN a.fk_subscription END) / b.Total_Subs AS Cohort_F
     , COUNT(DISTINCT CASE WHEN a.cohort = 'Cohort G (+3)' THEN a.fk_subscription END) / b.Total_Subs AS Cohort_G
-    , COUNT(DISTINCT CASE WHEN a.cohort = 'Cohort H (+4)' THEN a.fk_subscription END) / b.Total_Subs AS Cohort_H
 FROM VIEW_3C AS a
 LEFT JOIN VIEW_4 AS b
     ON a.fk_imported_at_date = b.fk_imported_at_date
@@ -212,11 +225,9 @@ SELECT Date--DATE_FORMAT(to_date(Date,'yyyyMMdd'),'dd-MMM-yyyy') AS Date
      , MAX(Cohort_D) AS Cohort_D
      , MAX(Cohort_E) AS Cohort_E
      , MAX(Cohort_F) AS Cohort_F
+     , MAX(Cohort_G) AS Cohort_G
      , published_date AS HMT_Date
 FROM VIEW_5 AS a
 CROSS JOIN VIEW_1B AS b
-GROUP BY 1,2,9
+GROUP BY 1,2,10
 ORDER BY 1,2
-
-
-
